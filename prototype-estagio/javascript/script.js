c01c050f4f92266b51c949460ad4244f8e10ab72
Rodrigo Santos <91631699+aqueledigola@users.noreply.github.com>
2022-05-30 21:54:45 -0300

    .

--- prototype-estagio/javascript/script.js
+++ prototype-estagio/javascript/script.js
@@ -1,3 +1,4 @@
+// MOSTRAR E ESCONDER POP-UP
 function showModal() {
     let obj = document.getElementById("modal");
     let blur = document.getElementById("all");
@@ -12,4 +13,23 @@ function hideModal() {
 
     obj.classList.remove("show-modal");
     blur.classList.remove("blur-all");
+}
+
+// MOSTRAR E ESCONDER SENHA
+function eyeClick() {
+    let inputSenha = document.getElementById("inputSenha")
+    let eye = document.getElementById("senhaonoff")
+    let senhaTypePassword = inputSenha.type == "password"
+
+    if (senhaTypePassword) {
+        inputSenha.setAttribute("type", "text")
+        eye.setAttribute("src", "img/dec_senhaoff.png")
+        eye.setAttribute("alt", "Ocultar Senha")
+
+    } else {
+        inputSenha.setAttribute("type", "password")
+        eye.setAttribute("src", "img/dec_senhaon.png")
+        eye.setAttribute("alt", "Mostrar Senha")
+
+    }
 }
