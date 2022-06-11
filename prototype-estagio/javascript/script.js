57962f01775ca5834aeba482f4cfac6d3d8b668f
Rodrigo Santos <91631699+aqueledigola@users.noreply.github.com>
2022-06-10 21:28:06 -0300

    .

--- prototype-estagio/javascript/script.js
+++ prototype-estagio/javascript/script.js
@@ -35,3 +35,13 @@ function eyeClick() {
 }
 
 // MOSTRAR E ESCONDER NAVBAR RESPONSIVO
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventdefault();
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu); 
