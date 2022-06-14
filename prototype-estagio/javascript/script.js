ddb5236bc395eeebc293b212fc4ff4199b32f12c
aqueledigola <rodrigo.santos.estudante@eteot.rio.br>
2022-06-14 11:44:27 -0300

    .

--- prototype-estagio/javascript/script.js
+++ prototype-estagio/javascript/script.js
@@ -1,20 +1,3 @@
-// MOSTRAR E ESCONDER POP-UP
-function showModal() {
-    let obj = document.getElementById("modal");
-    let blur = document.getElementById("all");
-
-    obj.classList.add("show-modal");
-    blur.classList.add("blur-all");
-}
-
-function hideModal() {
-    let obj = document.getElementById("modal");
-    let blur = document.getElementById("all");
-
-    obj.classList.remove("show-modal");
-    blur.classList.remove("blur-all");
-}
-
 // MOSTRAR E ESCONDER SENHA
 function eyeClick() {
     let inputSenha = document.getElementById("inputSenha");
